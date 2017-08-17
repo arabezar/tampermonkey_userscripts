341234a3f4d38a9951b2e5e5435fcf311ce9b388
hant0508 <hant0508@gmail.com>
2017-08-17 23:30:57 +0300

    Update regex

--- NNM Club and Rustorka ad remover.user.js
+++ NNM Club and Rustorka ad remover.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         NNM Club and Rustorka ad remover
 // @namespace    http://tampermonkey.net/
-// @version      1.0.3
+// @version      1.0.4
 // @description  Removes ad banners by marketgid/tovarro on nnmclub.to and rustorka.com (use with Ublock Origin or Adblock Plus)
 // @author       hant0508
 // @include      /^https?://nnmclub.to/*/
 // @include      /^https?://nnm-club.to/*/
 // @include      /^https?://nnm-club.me/*/
+// @include      /^https?://nnm-club.name/*/
 // @include      /^https?://rustorka.com/*/
 // @grant        none
 // ==/UserScript==
@@ -27,10 +28,14 @@ function rem()
 function xpath()
 {
     var time;
-    var regex1 = "/html/body//div[@id!=\"body_container\" and ./div/div/div/div/a[contains(@href, 'marketgid') or contains(@href, 'tovarro')]]";
-    var regex2 = "/html/body/*[1 and .//a[contains(@href, 'bgrndi.com') or contains(@href, 'traforet.com')]]";
-    var regex3 = "/html/body//div[@id=\"page_container\"]/div//opan[a]";
-    var regex = [regex1, regex2, regex3];
+//    var regex1 = "/html/body//div[@id!=\"body_container\" and ./div/div/div/div/a[contains(@href, 'marketgid') or contains(@href, 'tovarro')]]";
+//    var regex2 = "/html/body/*[1 and .//a[contains(@href, 'bgrndi.com') or contains(@href, 'traforet.com')]]";
+//    var regex3 = "/html/body//div[@id=\"page_container\"]/div//opan[a]";
+    var regex1 = "/html/body//div[@class=\"copyright\"]//iframe";
+    var regex2 = "/html/body//div[@class=\"wrap\"]//td/center/noindex/iframe";
+    var regex4 = "/html/body//div[@id=\"sidebar1-wrap\"]/iframe";
+    var regex5 = "/html/body//div[@class=\"post_body\"]//table//div/iframe";
+    var regex = [regex1, regex2, regex4, regex5];
 
     for (var i = 0; i < regex.length; i++)
         snapResults[i] = document.evaluate(regex[i], document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);

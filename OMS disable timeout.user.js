a96643f5829fdcc116e7123ee8ae5263abd3d5cb
hant0508 <hant0508@gmail.com>
2018-02-17 22:43:59 +0300

    Update OMS disable timeout.user.js

--- OMS disable timeout.user.js
+++ OMS disable timeout.user.js
@@ -1,25 +1,20 @@
 // ==UserScript==
 // @name         OMS disable timeout
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  Disables 30 seconds timer and removes ad banners on ru.onlinemschool.com (use with Ublock Origin or Adblock Plus)
 // @author       hant0508
 // @include      http://ru.onlinemschool.com/*
 // @grant        none
-// @ran-at       document-idle
 // ==/UserScript==
 
-function f() {
-  var path = "/html/body//div[@id and div [@style and (@onclick and img[@style]) or @style and div[@class and @style]] and @id != \"oms_r_center_2\"]";
-  var ad = document.evaluate(path, document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
-  if (ad.snapshotLength > 0) clearInterval(timer);
+function f ()
+{
   oms.ads = true;
-
-  for (var i = ad.snapshotLength - 1; i >= 0; i--){
-    var elm = ad.snapshotItem(i);
-    console.log(elm);
-    elm.parentNode.removeChild(elm);
-  }
+  oms.ads_detected = function(){return true;};
+  oms.ads_detect = function(){return true;};
 }
 
-var timer = window.setInterval(f, 50);
+var script = document.createElement('script');
+script.appendChild(document.createTextNode('('+ f +')();'));
+document.body.appendChild(script);

411f4e3c58d122b9d77568defd937439f5a6460c
Arabezar <jaaz@ya.ru>
2025-09-18 22:42:26 +0300

    Удалено устаревшие элементы, добавлены новые

--- Avito Cleaner.user.js
+++ Avito Cleaner.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Avito Cleaner
 // @namespace    http://tampermonkey.net/
-// @version      0.2.4
+// @version      0.2.5
 // @description  Unchecks commercial services and hides some intrusive ads
 // @author       hant0508
 // @include      https://www.avito.ru/*
@@ -26,31 +26,16 @@ function hideArr (arr) {
 }
 
 (function() {
-	var elm = document.getElementsByClassName('packages-tab_free')[0];
-	if (elm !== null && elm !== undefined) {
-		elm.closest('.packages-tab').click();
-	}
-
-	uncheck(document.getElementById('service-premium'));
-	uncheck(document.getElementById('service-up-x2'));
-	uncheck(document.getElementById('service-vip'));
-	uncheck(document.getElementById('service-highlight'));
-
-	hideArr(document.getElementsByClassName('profile-item-promo'));
-	hideArr(document.getElementsByClassName('profile-item-warning'));
-
 	// Removing top banner
-	var elms = document.querySelectorAll('*[class^="index-module-left-block-"]');
-	if (elms.length > 0) {
+	var elm = document.querySelector('h1[class^="styles-module-root-"]');
+	if (elm != null && elm != undefined) {
 		setTimeout(() => {
-			elm = elms[0].nextElementSibling.firstElementChild;
-			if (elm !== null && elm !== undefined &&
-			    elm.querySelectorAll('*[name="close"], *[class^="personal-discount-"], *[class*="Close-module-mask-"], *[class*="styles-module-closeIcon-"], *[class*="styles-module-icon-"]').length > 0) { hide(elm); }
+			// Removing top banner
+			hideArr(document.getElementsByClassName('cERYi'));
+			
+			// Remove cards under «Мои объявления»
+			elm = elm.nextElementSibling;
+			if (elm !== null && elm !== undefined && elm.className.includes('Cards-module-wrapper-')) { hide(elm); }
 		}, 2000);
-	}
-
-	elm = document.getElementsByClassName('js-packages')[0];
-	if (elm.children[1].innerHTML.includes('Примените к объявлению пакет услуг')) {
-		hide(elm);
 	}
 })();
